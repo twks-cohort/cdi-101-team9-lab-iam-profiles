3e7f6f95e88cf65520b1c62ac1934529f3dbef32
Masha B <masha.budryte@thoughtworks.com>
2023-08-03 10:42:37 +0100

    Team 9 - Masha/Shagufta - Initial setup for week 3

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.2"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 
@@ -10,7 +10,7 @@ terraform {
     hostname     = "app.terraform.io"
     organization = "twks-cohort"
     workspaces {
-      prefix = "blue-lab-iam-profiles-"
+      prefix = "cdi-101-team9-lab-iam-profiles-"
     }
   }
 }
@@ -21,14 +21,14 @@ provider "aws" {
   # this section commented out during the initial bootstrap run
   # once the assumeable roles are created, uncomment and change
   # secrethub.*.env to contain the appropriate service account identity
-  assume_role {
-    role_arn     = "arn:aws:iam::${var.aws_account_id}:role/${var.aws_account_role}"
-    session_name = "lab-iam-profiles"
-  }
+  # assume_role {
+  #   role_arn     = "arn:aws:iam::${var.aws_account_id}:role/${var.aws_account_role}"
+  #   session_name = "lab-iam-profiles"
+  # }
 
   default_tags {
     tags = {
-      pipeline = "blue-lab-iam-profiles"
+      pipeline = "cdi-101-team9-lab-iam-profiles"
     }
   }
 }
